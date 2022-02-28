c525009be7d592585d2e545d9ce75bdd823c3d86
b18jacha <b18jacha@student.his.se>
2022-02-28 15:49:53 +0100

    Uploaded project

---
+++ OOP2022_b18jacha_a19gabos_assignment2/src/b18jacha_a19gabos/ToDo.java
@@ -0,0 +1,19 @@
+package b18jacha_a19gabos;
+
+import javax.swing.JFrame;
+
+import se.his.it401g.todo.*;
+
+public class ToDo {
+
+    public static void main(String[] args) {
+    	JFrame frame = new JFrame();
+    
+    	Task task = new HomeTask();
+    	frame.add(task.getGuiComponent());
+    	
+    	frame.setBounds(2100,100,400,400);
+    	frame.setVisible(true);
+    	frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
---
+++ OOP2022_b18jacha_a19gabos_assignment2/src/module-info.java
@@ -0,0 +1,6 @@
+module OOP2022_b18jacha_a19gabos_assignment2 {
+    requires java.base;
+    requires java.se;
+    requires it401g.todo;
+	exports b18jacha_a19gabos;
+}
